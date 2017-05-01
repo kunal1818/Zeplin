46f67bfc5c53b539611a6e1bf3d32eb50e483cad
kunal1818 <kunalsharma1818@gmail.com>
2017-05-01 12:07:58 +0530

    drawer added

--- app/src/main/java/com/example/android/zeplin/MainActivity.java
+++ app/src/main/java/com/example/android/zeplin/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.android.zeplin;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.android.zeplin.fragement.HomeFragement;
 
@@ -12,11 +16,23 @@ import com.example.android.zeplin.fragement.HomeFragement;
  */
 public class MainActivity extends AppCompatActivity {
     private HomeFragement homeFragement = new HomeFragement();
+    private ImageView ivHeader;
+    private DrawerLayout dlSidemenu;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dlSidemenu = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ivHeader = (ImageView) findViewById(R.id.iv_header);
+        ivHeader.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+                dlSidemenu.openDrawer(Gravity.START);
+            }
+        });
+
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         ft.replace(R.id.framelayout, homeFragement);
